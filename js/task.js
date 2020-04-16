9ef56dc6fef834af3594122c74f2433f5a8f3a3e
nicktfranklin <nthompsonsonfranklin@gmail.com>
2020-04-16 14:12:59 -0400

    attempt to load new screen

--- js/task.js
+++ js/task.js
@@ -19,19 +19,34 @@
 
 // console.log(truncated_geometric(0.2, 25));
 // console.log(isotropic_norm(6));
-var A_l = [[1.06465446, 0.10682176], [-0.10682176, 1.06465446]];
-var process_init_l = 6.0;
+// var A_l = [[1.04867124, 0.21257618], [-0.21257618, 1.04867124]];
+
+// the recency bias (low noise) condition
+var A_l = [[ 1.08561432 , 0.23139049], [-0.23139049,  1.08561432]];
+var process_init_l = 3.5;
 var process_noise_l = 0.02;
-var observ_noise = 25.0
-var observ_scale = 15.0
-var p = 0.2
-var t_max = 20;
-var t_min = 5;
+
+// the high noise control condition
+var A_l = [[ 0.89887523,  0.04498125], [-0.04498125,  0.89887523]];
+var process_init_l = 6.0;
+var process_noise_l = 1.0;
+
+// the test condition
+var A_l = [[ 1.0358622 , 0.17172509], [-0.17172509,  1.0358622]];
+var process_init_l = 3.71428571;
+var process_noise_l = 0.29714286;
+
+
+var observ_noise = 10.0
+var observ_scale = 20.0
+var p = 0.25
+var t_max = 8;
+var t_min = 2;
 
 function generate_trial_parameters(
-  A_l, process_init_l, process_noise_l, observ_noise, observ_scale) {
+  A_l, process_init, process_noise, observ_noise, observ_scale) {
     var duration = truncated_geometric(p, t_max) + t_min;;
-    var x = sample_lds_states(A_l, process_init_l, process_noise_l, duration);
+    var x = sample_lds_states(A_l, process_init, process_noise, duration);
     var z = make_observations(x, observ_noise, observ_scale);
     
     // convert the sampled values to pixel space and present in usable format
@@ -69,7 +84,7 @@ function run_all_trials() {
     trial_parameters = queue_trials.shift();
 
     // default parameters
-    var dot_duration = 300;
+    var dot_duration = 500;
     var mask_duration = 500;
     var canvas_size = 350;
     var isi = 500;
